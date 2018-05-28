7d24e2889c59ce974ee3fc31ab08714d2c7743f2
Alessia Peviani <alecatz23@gmail.com>
2018-05-29 01:19:31 +0200

    Modified searchYelp to call Yelp.search() to obtain business info from Yelp API

--- src/App.js
+++ src/App.js
@@ -13,10 +13,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {businesses:[]};
+    this.searchYelp = this.searchYelp.bind(this);
   }
 
   searchYelp(term,location,sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term,location,sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+    //console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
   }
 
   render() {
@@ -24,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp}/>
-        <BusinessList businesses={businesses}/>
+        <BusinessList businesses={this.state.businesses}/>
       </div>
     );
   }
